33d0a532fd82b77714537620ff6e5efea9a4e2e2
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-03-14 11:17:29 +0100

    Added updates to controllers and models

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -10,8 +10,7 @@ export const register = async (req,res, next)=>{
         const hash = bcrypt.hashSync(req.body.password, salt)
 
         const newUser = new User({
-          username:req.body.username,
-          email:req.body.email,
+          ...req.body,
           password:hash,
         })
 
@@ -37,7 +36,7 @@ export const login = async (req,res, next)=>{
       //the code below removes the password and admin status from frontend view
       const {password, isAdmin, ...otherDetails} = user._doc
 
-      res.cookie("access_token", token, {httpOnly:true}).status(200).send({...otherDetails})
+      res.cookie("access_token", token, {httpOnly:true}).status(200).send({details:{...otherDetails}, isAdmin})
 
     }catch(err){
       next(err)
--- controllers/room-controllers.js
+++ controllers/room-controllers.js
@@ -80,3 +80,35 @@ export const getRooms = async (req, res, next)=>{
         next(err)
     }
 }
+
+
+// app.get("/todos", async (req, res) => {
+//     const todos = await Todo.find()
+
+//     res.json(todos)
+// })
+
+// app.post("/todos/new", (req, res) => {
+//     const todo = new Todo({
+//         text: req.body.text
+//     })
+
+//     todo.save()
+//     res.json(todo)
+// })
+
+// app.delete("/todos/delete/:id", async(req, res) => {
+//     const result = await Todo.findByIdAndDelete(req.params.id)
+
+//     res.json(result)
+// })
+
+// app.get("/todos/complete/:id", async (req, res)=> {
+//     const todo = await Todo.findById(req.params.id)
+
+//     todo.complete = !todo.complete
+    
+//     todo.save()
+
+//     res.json(todo)
+// })
